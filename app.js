661673de6d3d4e7ec6468196bae98788c999c47a
yuta yanagisawa <yuta.yanagisawa@soliton.co.jp>
2018-05-24 11:06:48 +0900

    add cron, add react to baj pm, add rich embed user info

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
 const Discord = require('discord.js');
 const logger = require('winston');
 const auth = require('./auth.json');
 const request = require('request-promise');
 const appInfo = require('./appinfo.json');
 const http = require('http');
+const _ = require('lodash');
+const CronJob = require('cron').CronJob;
 
-const PREFIX = "?!"
+const PREFIX = "?!";
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -20,6 +21,7 @@ var bot = new Discord.Client();
 //http post server
 var server = http.createServer( function (req, res) {
     if (req.method == 'POST') {
+
         let body = '';
         req.on('data', function (reqBody) {
             body = JSON.parse(reqBody)
@@ -43,11 +45,32 @@ bot.on('ready', function() {
     console.log("connected!");
 });
 
+// cron
+var job = new CronJob({
+    cronTime: '00 00 21 * * *',
+    onTick: function() {
+        bot.channels.get("446477206160408597").send("It's time to play boisss!");
+    },
+    start: false,
+    timeZone: 'Asia/Tokyo'
+});
+job.start();
+
 bot.on('message', function(message) {
     if (message.author.equals(bot.user)) return;
 
-    if (message.content.includes("baj")) {
-        message.channel.send(`${bot.emojis.find("name","salt")} ${bot.emojis.find("name","salt")} ${bot.emojis.find("name","salt")}`);
+    var salt = bot.emojis.find("name","salt");
+
+    if (message.author.equals(bot.users.find("id","393300171485609984"))) {
+        message.react(salt);
+    };
+
+    if (message.content.toLowerCase().includes("baj") || message.content.toLowerCase().includes("dot")) {
+        message.channel.send(`${salt} ${salt} ${salt}`);
+    }
+
+    if (message.content.includes("kim")) {
+        message.channel.send(`Move on move on din boiii, ${salt} ${salt} ${salt}`);
     }
 
     if (!message.content.startsWith(PREFIX)) return;
@@ -55,6 +78,25 @@ bot.on('message', function(message) {
     var args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0].toLowerCase()){
+        case "info": {
+            const embed = new Discord.RichEmbed();
+
+            embed.setTitle('User Info');
+            embed.setColor('#ff8d00');
+            embed.setAuthor(message.author.username, null, null);
+            embed.setThumbnail(message.author.avatarURL);
+            embed.setFooter("Made by Yuts", "https://cdn.discordapp.com/embed/avatars/0.png");
+            embed.addField("User", message.author.username, true);
+            embed.addField("ID", message.author.id, true);
+            embed.addField("Created", message.author.createdAt, true);
+            // embed.addField("Roles", _.map(message.member.roles, 'name').join(', '),false);
+            // console.log(message.member.roles.map());
+            message.channel.send(embed);
+        }
+            break;
+        case "gee":
+            message.channel.send(`let's play ${_.sample(appInfo.gameList)}!`);
+            break;
         case "ping":
             console.log(`Executed ${args[0]}`);
             message.channel.send("Pong!");
